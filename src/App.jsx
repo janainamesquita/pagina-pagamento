c86f046586a3c49be527e3426946a401b360ecd7
janainamesquita <janainammesquita@hotmail.com>
2025-04-25 11:47:58 -0300

    inicio

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import CardFront from "./components/frontCard";
+
+export default function App() {
+  return (
+    <div className="w-full h-screen flex">
+      <div className="w-[40%] relative h-full bg-[#271540]">
+        <div className="absolute top-10 left-60">
+          <CardFront />
+        </div>
+      </div>
+      <div className="w-[60%] h-full flex justify-end p-[40px]">
+        <h1 className="text-[40px] w-[70%] h-[150px] font-bold">Preencha os campos para concluir o pagamento</h1>
+      </div>
+
+    </div>
+  )
+}
---
+++ src/components/frontCard/index.jsx
@@ -0,0 +1,30 @@
+import { FcSimCardChip } from "react-icons/fc";
+import { LuNfc } from "react-icons/lu";
+
+export default function CardFront() {
+    return (
+        <div className="w-[600px] h-[350px] bg-black rounded-xl">
+            <div className="w-full h-[30%] flex">
+                <div className="w-[50%] h-full flex items-center pl-4 gap-2">
+                    <div className="w-[70px] h-[70px] rounded-full bg-white"></div>
+                    <div className="w-[50px] h-[50px] rounded-full bg-white"></div>
+                </div>
+                <div className="w-[50%] h-full flex p-4 justify-end">
+                    <p className="text-[20px] text-white">Nome do Banco</p>
+                </div>
+            </div>
+            <div className="w-full h-[40%] flex flex-col">
+                <div className="w-full h-[60%] flex items-center pl-2">
+                    <FcSimCardChip size={90}/> 
+                    <LuNfc size={35} color="#fff"/>
+                </div>
+                <div className="w-full h-[40%] pl-4">
+                    <p className="text-[45px] text-gray-500">0000 0000 0000 0000</p>
+                </div>
+            </div>
+            <div className="w-full h-[30%] pl-4">
+                <p className="text-white text-[45px]">Nome no Cartão</p>
+            </div>
+        </div>
+    )
+}
